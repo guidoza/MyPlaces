444453843a9a95902e03195ceff1246c24a6e360
Guillermo <guidoza7@gmail.com>
2015-04-22 12:58:36 +0200

    Tres primeras activitys

--- app/src/main/java/com/example/gui/myplaces/MainActivity.java
+++ app/src/main/java/com/example/gui/myplaces/MainActivity.java
@@ -1,9 +1,9 @@
 package com.example.gui.myplaces;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -14,26 +14,19 @@ public class MainActivity extends ActionBarActivity {
         setContentView(R.layout.activity_main);
     }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+    public void onClickDashboard(View view){
+        int id = view.getId();
+        if(id == R.id.buttonNuevoSitio) {
+            Intent i = new Intent(this, NuevoSitio.class);
+            startActivity(i);
+        } else if(id == R.id.buttonMisSitios){
+            Intent i = new Intent(this, MisSitios.class);
+            startActivity(i);
+        } else if(id == R.id.buttonCredits){
+            Intent i = new Intent(this, Credits.class);
+            startActivity(i);
         }
-
-        return super.onOptionsItemSelected(item);
     }
+
+
 }
